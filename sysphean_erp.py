e6ee6fbea392d34306e11b3721cfa3b910601c40
EissaMD <eissa.code@gmail.com>
2023-07-29 19:59:43 +0300

    Adding LeftMenu

--- sysphean_erp.py
+++ sysphean_erp.py
@@ -9,10 +9,15 @@ class App(ttk.Window):
             resizable=(True, False),
         )
         # create the main menu
-        self.columnconfigure((0),weight=1)
+        self.columnconfigure((1),weight=1)
         menu_ls = ("Accounting", "Sales" , "Inventory" , "Manufacturing")
         main_menu = MainMenu(self,menu_ls)
-        main_menu.grid(row=0,column=0, sticky="nswe" )
+        main_menu.grid(row=0,column=0, columnspan=2, sticky="nswe" )
+        # create the secondary menu
+        self.rowconfigure((1),weight=1)
+        menu_ls = ("Accounting", "Sales" , "Inventory" , "Manufacturing")
+        main_menu = LeftMenu(self,menu_ls)
+        main_menu.grid(row=1,column=0,sticky="nswe")
 ##############################################################################################################
 
 class MainMenu(ttk.Frame):
@@ -21,6 +26,15 @@ class MainMenu(ttk.Frame):
         for menu in menu_ls:
             ttk.Button(self,text=menu ,bootstyle="light").pack(side="left")
 ##############################################################################################################
+
+class LeftMenu(ttk.Frame):
+    def __init__(self,master,menu_ls):
+        super().__init__(master,bootstyle="secondary",width=100 )
+        
+        for menu in menu_ls:
+            ttk.Button(self,text=menu ,bootstyle="secondary").pack(fill="x" , pady=4)
+##############################################################################################################
+
 
 if __name__ == "__main__":
     app = App()
