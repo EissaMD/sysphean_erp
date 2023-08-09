77f54dfa67c7d6c201b93aa77f298aa2e1023162
EissaMD <eissa.code@gmail.com>
2023-08-09 12:13:59 +0300

    Fix infotable btns (+/-) issue 'click do nothing'

--- resources/InfoTable.py
+++ resources/InfoTable.py
@@ -6,17 +6,17 @@ class InfoTable(ttk.Frame):
         super().__init__(master,)
         self.data = {} # initialize empty tree
         self.treeview = ttk.Treeview(self, columns=headers, show="headings" , bootstyle="primary" , height=3,)
-        self.treeview.pack(fill="both",expand=True,side="left")
+        self.treeview.pack(fill="both",side="left")
         for header in headers:
             label = header.replace("_", " ")
             label = label.capitalize()
             self.treeview.heading(header, text=label)
+        self.add_btn = self.remove_btn = lambda : 0
     ###############        ###############        ###############        ###############
     def add_remove_btn(self):
         frame = ttk.Frame(self) ; frame.pack(side="left",fill="both")
-        self.add_btn = self.remove_btn = lambda : 0
         ttk.Button(frame,text="+" , bootstyle="outline-primary", command=self.add_btn).pack(fill="both")
-        ttk.Button(frame,text="-" , bootstyle="outline-primary", command=self.remove_btn).pack(fill="both")
+        ttk.Button(frame,text="-" , bootstyle="outline-primary", command=self.delete_selection).pack(fill="both")
     ###############        ###############        ###############        ###############
     def clear(self):
         self.data = []
@@ -35,6 +35,6 @@ class InfoTable(ttk.Frame):
             self.add_rows(rows)
     ###############        ###############        ###############        ###############
     def delete_selection(self):
-        for sel_item in self.selection():
+        for sel_item in self.treeview.selection():
             self.treeview.delete(sel_item)
             self.data.pop(sel_item)
