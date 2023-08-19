262ff34e39e19a8d4cb42f9e851c0e231076a022
EissaMD <eissa.code@gmail.com>
2023-08-19 12:36:23 +0300

    Fix issue that rows entered to databse not saved, ans:commit()

--- resources/Logics/config_DB.py
+++ resources/Logics/config_DB.py
@@ -5,21 +5,23 @@ class DB():
     ###############        ###############        ###############        ###############
     def connect(self):
         try:
-            DB.conn = sqlite3.connect('sql.db')
+            DB.conn = sqlite3.connect(r'./sql.db')
             DB.cursor = DB.conn.cursor()
         except sqlite3.Error as error:
             messagebox.showerror("Error",f"Error in connection to sql database {error}")
     ###############        ###############        ###############        ###############
-    def insert(self,table,columns,values,a=9):
+    def insert(self,table,columns,values):
         col = ", ".join(columns)
         qq = ", ".join(["?" for i in range(len(values))])
         values = tuple(values)
         query = f'INSERT INTO {table} ({col}) VALUES ({qq})'
-        DB.cursor.execute(query, values)
-
+        try:
+            DB.cursor.execute(query, values)
+            DB.conn.commit()
+        except sqlite3.Error as error:
+            messagebox.showerror("Error",f"The system wasn't able to enter this data, {error.args}")
 
 
 if __name__ == "__main__":
     db = DB()
     db.connect()
-    db.cursor.execute('INSERT INTO customer (name) VALUES (?)')
