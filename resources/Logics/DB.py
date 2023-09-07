c78485d89497f5cc00d01e00aeebd66ab52cd34c
EissaMD <eissa.code@gmail.com>
2023-09-07 15:46:51 +0300

    Add description to each method in DB class

--- resources/Logics/DB.py
+++ resources/Logics/DB.py
@@ -5,6 +5,11 @@ import os
 class DB():
     ###############        ###############        ###############        ###############
     def connect(self,db_path=r'./sql.db'):
+        """Connect to database , Execute this function only one time.
+
+        Args:
+            db_path (regexp, optional): Path to database. Defaults to r'./sql.db'.
+        """
         if not os.path.exists(db_path):
             messagebox.showerror("Error",f"The database does not exist, please try to contact Sysphean support.")
             return
@@ -15,6 +20,13 @@ class DB():
             messagebox.showerror("Error",f"Error in connection to sql database {error}")
     ###############        ###############        ###############        ###############
     def insert(self,table,columns,values):
+        """insert a new row into the database
+
+        Args:
+            table (str): name of the table to insert data
+            columns (tuple): name of columns in the table 
+            values (tuple): values of the new row , no of columns SHOULD equal number of values
+        """
         col = ", ".join(columns)
         qq = ", ".join(["?" for i in range(len(values))])
         values = tuple(values)
@@ -26,6 +38,17 @@ class DB():
             messagebox.showerror("Error",f"The process couldn't be completed by the system, {error}")
     ###############        ###############        ###############        ###############
     def select(self,table,columns,conditions="",values=()):
+        """Get data from the database
+
+        Args:
+            table (str): name of the table to get data
+            columns (tuple): name of columns in the table 
+            conditions (str, optional): to filter records based on conditions. Defaults to "" no filter.
+            values (tuple, optional): if there are conditions need values. Defaults to ().
+
+        Returns:
+            list: a list of tuples containing selected values
+        """
         col = ", ".join(columns)
         query = f"SELECT {col} FROM {table}"
         if conditions:
@@ -37,6 +60,14 @@ class DB():
             messagebox.showerror("Error",f"The process couldn't be completed by the system, {error}")
     ###############        ###############        ###############        ###############
     def update(self,table,columns,conditions,values):
+        """Update any records in database that meet the conditions
+
+        Args:
+            table (str): name of the table to update data
+            columns (tuple): name of columns in the table 
+            conditions (str): filter out the records
+            values (tuple): if there are conditions need values
+        """
         columns = [column+"=? " for column in columns]
         col = ", ".join(columns)
         query = f"UPDATE {table} SET {col}" + "WHERE " +conditions + ";"
@@ -47,6 +78,13 @@ class DB():
             messagebox.showerror("Error",f"The process couldn't be completed by the system, {error}")
     ###############        ###############        ###############        ###############
     def delete(self,table,conditions,values):
+        """Delete any records from database
+
+        Args:
+            table (str): name of the table to update data
+            conditions (str): filter out the records
+            values (tuple): if there are conditions need values
+        """
         query = f"DELETE FROM {table}"
         if conditions:
             query = query + " WHERE " + conditions
@@ -58,6 +96,14 @@ class DB():
             messagebox.showerror("Error",f"The process couldn't be completed by the system, {error}")
     ###############        ###############        ###############        ###############
     def get_last_id(self,table):
+        """last id returned from any table
+
+        Args:
+            table (str): table name
+
+        Returns:
+            int: last ID
+        """
         self.cursor.execute(f"SELECT id from {table} order by id DESC limit 1")
         last_id = self.cursor.fetchone()
         return last_id[0] if last_id else 0
