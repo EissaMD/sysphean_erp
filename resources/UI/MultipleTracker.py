7fb52fca2af556651e2303cd6eacb2d1e4802354
EissaMD <eissa.code@gmail.com>
2023-11-26 16:12:44 +0300

    MultipleTracker: single item in ls won't display dropdown menu

--- resources/UI/MultipleTracker.py
+++ resources/UI/MultipleTracker.py
@@ -5,12 +5,13 @@ class MultipleTracker(ctk.CTkFrame):
     def __init__(self,master , select_ls=["First" , "Secound" , "Third"]):
         super().__init__(master=master)
         self.pack(fill="both",expand=True)
-        frame = ctk.CTkFrame(self,fg_color="transparent", border_width=2)
-        frame.pack(fill="x" ,pady=5,padx=4)
-        ctk.CTkLabel(frame , text=f"Select from menu: " , font=("arial",14), width=200).pack(side="left" ,anchor=ctk.W , pady=4,padx=10)
-        self.menu = ctk.CTkOptionMenu(frame, values=select_ls, width=200 , command=self.select_item
-                                    , fg_color="#565e58",button_color="#565e58",button_hover_color="#3c423e" , corner_radius=0 )
-        self.menu.pack(side="left", fill="x" )
+        if len(select_ls) > 1:
+            frame = ctk.CTkFrame(self,fg_color="transparent", border_width=2)
+            frame.pack(fill="x" ,pady=5,padx=4)
+            ctk.CTkLabel(frame , text=f"Select from menu: " , font=("arial",14), width=200).pack(side="left" ,anchor=ctk.W , pady=4,padx=10)
+            self.menu = ctk.CTkOptionMenu(frame, values=select_ls, width=200 , command=self.select_item
+                                        , fg_color="#565e58",button_color="#565e58",button_hover_color="#3c423e" , corner_radius=0 )
+            self.menu.pack(side="left", fill="x" )
         self.tracker_body = ctk.CTkFrame(self,fg_color="transparent", border_width=2)
         self.tracker_body.pack(fill="both",expand=True ,pady=5,padx=4)
         self.select_item(select_ls[0])
