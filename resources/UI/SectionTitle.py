ee1717d38e7a594c76ef1ad903127bfd7f740351
EissaMD <eissa.code@gmail.com>
2023-11-28 09:54:42 +0300

    SectionTitle: Remove top&bottom Separator and added to right side

--- resources/UI/SectionTitle.py
+++ resources/UI/SectionTitle.py
@@ -2,15 +2,12 @@ import customtkinter as ctk
 import tkinter.ttk as ttk
 
 class SectionTitle(ctk.CTkFrame):
-    def __init__(self,master , text="text",separator_top=False,separator_bottom=False,side="top"):
+    def __init__(self,master , text="text"):
         super().__init__(master=master ,fg_color="transparent" )
         self.pack(fill="x")
-        if separator_top:
-            ttk.Separator(self, orient="horizontal").pack(pady=(4,0),fill="x",padx=2)
         frame = ctk.CTkFrame(self,fg_color="transparent"); frame.pack(fill="x")
-        ctk.CTkLabel(frame,text=text,font=("Roboto Medium", 15)).pack(side=side , padx=20)
-        if separator_bottom:
-            ttk.Separator(self, orient="horizontal").pack(pady=(0,4),fill="x",padx=2)
+        ctk.CTkLabel(frame,text=text,font=("Roboto Medium", 15)).pack(side="left" , padx=20)
+        ttk.Separator(frame, orient="horizontal").pack(pady=10,fill="x",padx=2 , side="left",expand=True)
 ##############################################################################################################
 
 if __name__ == "__main__":
