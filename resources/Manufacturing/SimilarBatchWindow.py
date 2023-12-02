65c41cad83c0799b9d6996ec0bb146cd77783ad1
EissaMD <eissa.code@gmail.com>
2023-12-02 16:13:24 +0300

    Create SimilarBatchWindow class to check if the batch has been entered before

---
+++ resources/Manufacturing/SimilarBatchWindow.py
@@ -0,0 +1,33 @@
+from ..Logics import DB
+import customtkinter as ctk
+from tksheet import Sheet
+
+class SimilarBatchWindow(ctk.CTkToplevel):
+    def __init__(self,data):
+        # Checker : If there is similar batch..
+        columns = ("Id", "part_no", "quantity" ,"date_code" ,"remarks" ,"additional_info" ,"user_name" ,"time")
+        sealed_records = DB.select("entry_tracker",columns,"part_no = %s AND date_code=%s",tuple(data.values()))
+        if len(sealed_records) == 0:
+            return
+        super().__init__()
+        self.title("Similar Batch")
+        self.geometry("1000x310")
+        # Title
+        ctk.CTkLabel(master=self,text="Warning: There is similar batch in database, do you want to enter this batch anyway?"
+                               ).pack( pady=10, anchor="w", padx=10 )
+        # table frame
+        self.sheet = Sheet(self,
+                           headers=["ID", "Part No", "quantity", "Date Code", "Remarks", "Additional Info","User Name" ,"Time" ],
+                           data =sealed_records)
+        self.sheet.enable_bindings("single_select","drag_select","select_all","column_select","row_select","column_width_resize","double_click_column_resize","arrowkeys","row_height_resize","double_click_row_resize")
+        self.sheet.pack(padx=5,pady=2,expand=True,fill="x")
+        col_size =100
+        col_size= [col_size*0.5,col_size*2.5,col_size*0.5,col_size*0.65,col_size*1,col_size*1,col_size*1,col_size*1.2]
+        self.sheet.set_column_widths(column_widths = col_size)
+        frame = ctk.CTkFrame(self,fg_color="transparent", border_width=2)
+        ctk.CTkButton(self, text="Continue ?",command=self.continue_btn,width=150).pack(pady=2,)
+        self.deiconify()
+        self.wm_protocol("WM_DELETE_WINDOW", self.destroy)
+        self.wait_window(self)
+    def continue_btn(self):
+        print("Continue")
